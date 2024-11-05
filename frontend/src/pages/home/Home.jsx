574331a3fc5e9443f30bcad1fd9ea84e9f4063b5
Victory Chibueze Akunna <victorychibuakunna@gmail.com>
2024-11-05 13:44:24 +0100

    new development

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Banner from "./Banner"
-import Topsale from "./topsale"
+
 
 const Home = () => {
   return (
     <>
        <Banner/>
-       <Topsale/>
+
     </>
   )
 }
